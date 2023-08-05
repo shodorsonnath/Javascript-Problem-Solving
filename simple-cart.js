12f943e7b7419b6d74c6068501821c7b9fabad73
shodorsonnath <shodorsonnath@gmail.com>
2023-08-05 23:54:44 +0600

    simple cart

--- simple-cart.js
+++ simple-cart.js
@@ -1,1 +1,19 @@
 // This is a simple javascript problem calculation cart item
+const shoppingCart = [
+    {name: 'shoe', price: 1200, quantity: 2},
+    {name: 'shirt', price: 2200, quantity: 3},
+    {name: 'pant', price: 3700, quantity: 5},
+    {name: 'belt', price: 700, quantity: 7}
+];
+function totalCost(products){
+    let sum = 0;
+    for(let i = 0; i < products.length; i++){
+        const product = products[i];
+        const productTotal = product.price * product.quantity;
+        sum = sum + productTotal;
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('Total expense: ', expense);
