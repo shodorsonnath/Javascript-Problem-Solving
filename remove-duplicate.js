be234591dbcbc038c1f9e6c2c45b50e7ef8b0e76
shodorsonnath <shodorsonnath@gmail.com>
2023-08-05 23:06:29 +0600

    remove duplicate

---
+++ remove-duplicate.js
@@ -0,0 +1,10 @@
+// This is a simple javascript problem remove duplicate item from array
+function removeDuplicate(names){
+    for(let i = 0; i <names.length; i++){
+
+    }
+}
+
+const names = ['sakib', 'liton', 'sakib', 'virat']
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames);
