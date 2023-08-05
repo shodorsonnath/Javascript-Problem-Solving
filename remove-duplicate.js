d8a40a6c8f6abd382d277360700a9c6136d4f44f
shodorsonnath <shodorsonnath@gmail.com>
2023-08-05 23:18:25 +0600

    number-problem

--- remove-duplicate.js
+++ remove-duplicate.js
@@ -1,8 +1,13 @@
 // This is a simple javascript problem remove duplicate item from array
 function removeDuplicate(names){
+    const unique = [];
     for(let i = 0; i <names.length; i++){
-
+        const name =names[i];
+        if(unique.includes(name) === false){
+            unique.push(name);
+        }
     }
+    return unique;
 }
 
 const names = ['sakib', 'liton', 'sakib', 'virat']
